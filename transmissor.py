92a05101c11179363892e4262eaa60e267b73680
samuelandradematos <sammattos@aluno.unb.br>
2025-07-03 17:52:43 -0300

    Correções em receptor e transmissor

--- transmissor.py
+++ transmissor.py
@@ -1,26 +1,21 @@
 import socket as skt
-from camadaFisica import CamadaFisica
-
-
 
 class Transmissor:
-    def __init__(self, host, port):
+    def __init__(self):
         """!
         Inicializa o transmissor com o endereço do servidor.
         @param host: Endereço IP do servidor
         @param port: Porta do servidor
         """
-        self.host = host
-        self.port = port
         self.sock = None
 
-    def connect(self):
+    def connect(self, server_address, server_port):
         """!
         Conecta ao servidor.
         @return: Objeto socket conectado ao servidor
         """
         self.sock = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
-        self.sock.connect((self.host, self.port))
+        self.sock.connect((server_address, server_port))
 
     def sendmsg(self,msg):
         """!
@@ -36,8 +31,8 @@ class Transmissor:
         return recvmsg.decode('utf-8')
 
 if __name__ == '__main__':
-    transmissor = Transmissor('localhost', 64000)
-    transmissor.connect()
+    transmissor = Transmissor()
+    transmissor.connect('localhost', 64000)
     mensagem = "Olá, servidor!"
     print(f"Enviando mensagem: {mensagem}")
     resposta = transmissor.sendmsg(mensagem)
